5439681c6aaf2a0e2779e7a56b75a6cdaafbd419
Fariz313 <fariz313.akbar@gmail.com>
2024-06-09 04:40:55 +0700

    Safe Point Create Branch & Bound algoritm

--- branchbound.js
+++ branchbound.js
@@ -1,3 +1,4 @@
+
 const datasetPekerja = [
     {
         "nama": "budi",
@@ -21,22 +22,135 @@ const datasetPekerja = [
     }
 ];
 let treeArray = [];
+
+/**
+ * @param {Array} dataset - Parameter berisi dataset yang diolah.
+ */
 function branchbound(dataset) {
     const firstLb = lbCalulate(dataset)
-    treeArray.push({ id: 0, ...firstLb })
+    let id = 0
+    treeArray.push({ id, children: null, level: 0, address: [], fixValue: [], ...firstLb })
     console.log(treeArray);
+    let leafData = [];
+    getLeaf(treeArray, leafData);
+    leafData.forEach((element, index) => {
+        const levelElement = element.level + 1
+        const address = element.address
+        for (let index2 = 0; index2 < dataset.length; index2++) {
+            // console.log("index2 = ", index2);
+            const lb = lbCalulate(dataset, index2, address, element.fixValue)
+            // console.log(lb);
+            if (!element.children) {
+                element.children = []
+            }
+            let addressElement = [...address, index2]
+            element.children.push({ id: ++id, ...lb, children: null, level: levelElement, address: addressElement })
+        }
+    });
+    console.log("yg ini", treeArray);
+    leafData = [];
+    getLeaf(treeArray, leafData);
+    let minLeaf = getMinLbForAll(leafData);
+    console.log(minLeaf, "inLeaf");
+    const levelElement = minLeaf.level + 1
+    const address = minLeaf.address
+    for (let index2 = 0; index2 < dataset.length; index2++) {
+        console.log("index2 = ", index2);
+        const lb = lbCalulate(dataset, index2, address, minLeaf.fixValue, true)
+        console.log(lb);
+        if (!minLeaf.children) {
+            minLeaf.children = []
+        }
+        let addressElement = [...address, index2]
+        if (lb) {
+            minLeaf.children.push({ id: ++id, ...lb, children: null, level: levelElement, address: addressElement })
+        }
+    }
+    console.log("yg ini", treeArray);
+    leafData = [];
+    getLeaf(treeArray, leafData);
+    minLeaf = getMinLbForAll(leafData);
+    console.log(minLeaf, "inLeaf");
+
 }
 
-function lbCalulate(dataset, ...fixValue) {
+/**
+ * @param {Array} dataset - Parameter berisi dataset yang diolah.
+ * @param {int} indexValue - Parameter berisi nilai index dari value yang akan dimasukan.
+ * @param {Array} address - Parameter array alamat index dari tree.
+ * @param {Array} fixValueIndex - Parameter nilai fix yang ada di parentnya.
+ * @param {Boolean} debug - Parameter nilai untuk debug.
+ */
+function lbCalulate(dataset, indexValue = null, address, fixValueIndex, debug) {
     let lb = 0
     let lbSource = []
-    dataset.forEach(element => {
-        const minValue = Math.min(...element.cost)
-        lbSource.push(minValue);
-        lb += minValue;
+    dataset.forEach((element, index) => {
+        if (indexValue === null) {
+            const minValue = Math.min(...element.cost)
+            lbSource.push(minValue);
+            lb += minValue;
+        } else {
+            let data = element
+            const level = address.length
+            minValue = null
+            element.cost.forEach((el, idx) => {
+                if (index == level) {
+                    if (idx == indexValue && !address.includes(idx)) {
+                        minValue = el
+                    }
+                } else if (index < level) {
+                    if (idx == address[index]) {
+                        minValue = el
+                    }
+                } else {
+                    if (!address.includes(idx) && idx != fixValueIndex) {
+                        if (!minValue) {
+                            minValue = el
+                        } else if (minValue > el) {
+                            minValue = el
+                        }
+                    }
+
+                }
+            });
+            lbSource.push(minValue);
+            lb += minValue;
+        }
     });
+    if(lbSource.includes(null)){
+        return null;
+    }
     const lbObject = { lbSource, lb }
     return lbObject
+}
+
+/**
+ * @param {Array} dataset - Parameter berisi dataset yang diolah.
+ * @param {Array} output - Parameter output akan mendapat setiap leaf paling ujung.
+
+ */
+function getLeaf(dataset, output) {
+    dataset.forEach(element => {
+        if (element.children) {
+            getLeaf(element.children, output)
+        } else {
+            output.push(element)
+        }
+    });
+}
+
+function getMinLbForAll(data) {
+    let minlb = null;
+    data.forEach(element => {
+        if (!minlb) {
+            minlb = element
+        } else if (minlb.lb > element.lb) {
+            minlb = element
+        }else if (minlb.lb == element.lb && minlb.level<element.level) {
+            minlb = element
+        }
+    });
+    return minlb;
 }
 
 branchbound(datasetPekerja);
